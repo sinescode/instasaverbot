8ca82afa682b45272dd939c41fb51ea663b4851a
Sines code <simplenecessarycode@gmail.com>
2025-09-07 22:27:39 +0600

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def webhook():
                 excel_file = json_to_excel(input_path)
                 
                 # Send the Excel file
-                send_document(chat_id, excel_file)
+                send_document(chat_id, excel_file,"")
                 
                 # Clean up
                 try:
