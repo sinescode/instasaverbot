27018c949ab560f111305157acd6574a772269be
Sines code <simplenecessarycode@gmail.com>
2025-09-22 12:14:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -185,5 +185,6 @@ def download_json():
 if __name__ == '__main__':
     with app.app_context():
         initialize()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
+    
     
